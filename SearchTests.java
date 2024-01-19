dbc03823f10e6b1267cc4b5ae1ea611ccd5ea6b9
YanaYuzhakova <yanochkayuzkova@yandex.ru>
2024-01-19 23:21:22 +0500

    hw 1 - изм 1

--- SearchTests.java
+++ SearchTests.java
@@ -8,7 +8,10 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
+        open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+        //изменеие 1
     }
 }
